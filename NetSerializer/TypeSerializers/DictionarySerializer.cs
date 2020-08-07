b01e0df7690f202c72d748ba9518963a442e46b6
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-08-07 19:31:21 +0200

    Fix dictionary null serialization

--- NetSerializer/TypeSerializers/DictionarySerializer.cs
+++ NetSerializer/TypeSerializers/DictionarySerializer.cs
@@ -148,6 +148,11 @@ namespace NetSerializer
 
 		public static void WritePrimitive<TKey, TValue>(Serializer serializer, Stream stream, Dictionary<TKey, TValue> value)
 		{
+			if (value == null)
+			{
+				return;
+			}
+			
 			var kvpArray = new KeyValuePair<TKey, TValue>[value.Count];
 
 			int i = 0;
@@ -160,6 +165,12 @@ namespace NetSerializer
 		public static void ReadPrimitive<TKey, TValue>(Serializer serializer, Stream stream, out Dictionary<TKey, TValue> value)
 		{
 			var kvpArray = (KeyValuePair<TKey, TValue>[])serializer.Deserialize(stream);
+
+			if (kvpArray == null)
+			{
+				value = null;
+				return;
+			}
 
 			value = new Dictionary<TKey, TValue>(kvpArray.Length);
 
